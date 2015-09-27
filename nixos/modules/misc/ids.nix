e4dfdbbe31dc67ee4b01f8db7aca6c223a01b2c0
aszlig <aszlig@redmoonstudios.org>
2015-09-27 15:33:30 +0200

    nixos: Rename uid and add gid for "taskd".
    I'm renaming the attribute name for uid, because the user name is called
    "taskd" so we should really use the same name for it.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- nixos/modules/misc/ids.nix
+++ nixos/modules/misc/ids.nix
@@ -234,6 +234,7 @@
       #lxd = 210; # unused
       kibana = 211;
       xtreemfs = 212;
+      taskd = 213;
 
       # When adding a uid, make sure it doesn't match an existing gid. And don't use uids above 399!
 
@@ -446,7 +447,7 @@
       lxd = 210; # unused
       #kibana = 211;
       xtreemfs = 212;
-      taskserver = 213;
+      taskd = 213;
 
       # When adding a gid, make sure it doesn't match an existing
       # uid. Users and groups with the same name should have equal

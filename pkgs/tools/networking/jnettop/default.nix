cb0529a1d74321e8834c7e79557e1db02e4ef0c7
Matthias Beyer <mail@beyermatthias.de>
2019-07-29 16:20:07 +0200

    Remove link: is dead (domain not found) for more than a month.

--- pkgs/tools/networking/jnettop/default.nix
+++ pkgs/tools/networking/jnettop/default.nix
@@ -30,7 +30,6 @@ stdenv.mkDerivation rec {
       by bandwidth they use.
     '';
 
-    homepage = http://jnettop.kubs.info/;
     license = stdenv.lib.licenses.gpl2Plus;
     platforms = stdenv.lib.platforms.unix;
   };

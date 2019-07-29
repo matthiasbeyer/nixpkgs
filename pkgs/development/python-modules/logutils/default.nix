5ccd591d0e80c0ca3f02fc611a75698e225d2c07
Matthias Beyer <mail@beyermatthias.de>
2019-07-29 16:12:25 +0200

    Remove link: points to Google Code which was discontinued

--- pkgs/development/python-modules/logutils/default.nix
+++ pkgs/development/python-modules/logutils/default.nix
@@ -14,7 +14,6 @@ buildPythonPackage rec {
 
   meta = with stdenv.lib; {
     description = "Logging utilities";
-    homepage = http://code.google.com/p/logutils/;
     license = licenses.bsd0;
   };
 

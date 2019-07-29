f79c050d761b3e4d8d46b3716143ea1e6f943677
Matthias Beyer <mail@beyermatthias.de>
2019-07-29 16:20:47 +0200

    Remove link: is dead (connect timeout) for more than a month.

--- pkgs/development/libraries/fcgi/default.nix
+++ pkgs/development/libraries/fcgi/default.nix
@@ -24,7 +24,6 @@ stdenv.mkDerivation rec {
 
   meta = with stdenv.lib; {
     description = "A language independent, scalable, open extension to CG";
-    homepage = http://www.fastcgi.com/;
     license = "FastCGI see LICENSE.TERMS";
     platforms = platforms.all;
   };

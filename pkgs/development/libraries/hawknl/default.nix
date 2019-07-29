34606f83a5a498d7126657b98b74ba59cf346c67
Matthias Beyer <mail@beyermatthias.de>
2019-07-29 16:20:04 +0200

    Remove link: is dead (HTTP error 404) for more than a month.

--- pkgs/development/libraries/hawknl/default.nix
+++ pkgs/development/libraries/hawknl/default.nix
@@ -22,7 +22,6 @@ stdenv.mkDerivation {
   '';
 
   meta = {
-    homepage = http://hawksoft.com/hawknl/;
     description = "Free, open source, game oriented network API";
     license = stdenv.lib.licenses.lgpl2Plus;
     platforms = stdenv.lib.platforms.linux;

2b00d26ede2f036272c2aec9c929184d5d64ead5
Matthias Beyer <mail@beyermatthias.de>
2019-07-29 16:19:55 +0200

    Remove link: is a permanent redirect to "https://docs.pylonsproject.org/" and should be updated

--- pkgs/development/python-modules/pyramid_exclog/default.nix
+++ pkgs/development/python-modules/pyramid_exclog/default.nix
@@ -17,7 +17,6 @@ buildPythonPackage rec {
 
   meta = with stdenv.lib; {
     description = "A package which logs to a Python logger when an exception is raised by a Pyramid application";
-    homepage = http://docs.pylonsproject.org/;
     license = licenses.bsd0;
     maintainers = with maintainers; [ domenkozar ];
   };

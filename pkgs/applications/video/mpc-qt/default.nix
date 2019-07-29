654a40ad78bda28da74d423e0cc46207495c3013
Matthias Beyer <mail@beyermatthias.de>
2019-07-29 16:21:22 +0200

    Remove link: is dead (HTTP error 404) for more than a month.

--- pkgs/applications/video/mpc-qt/default.nix
+++ pkgs/applications/video/mpc-qt/default.nix
@@ -19,7 +19,6 @@ stdenv.mkDerivation rec {
 
   meta = with stdenv.lib; {
     description = "Media Player Classic Qute Theater";
-    homepage = https://github.com/cmdrkotori/mpc-qt;
     license = licenses.gpl2;
     platforms = platforms.unix;
     maintainers = with maintainers; [ romildo ];

91b8c8102eebd8d4d0d56edd720e04beb1f8860f
Matthias Beyer <mail@beyermatthias.de>
2019-07-29 16:19:49 +0200

    Remove link: is dead (HTTP error 404) for more than a month.

--- pkgs/servers/irc/charybdis/default.nix
+++ pkgs/servers/irc/charybdis/default.nix
@@ -28,7 +28,6 @@ stdenv.mkDerivation rec {
 
   meta = with stdenv.lib; {
     description = "IRCv3 server designed to be highly scalable";
-    homepage    = http://atheme.org/projects/charybdis.html;
     license     = licenses.gpl2;
     maintainers = with maintainers; [ lassulus fpletz ];
     platforms   = platforms.unix;

448eeb6dc2b321f1d8d8ff8cde9910191948328d
Matthias Beyer <mail@beyermatthias.de>
2019-07-29 16:20:27 +0200

    Remove link: is dead (connect timeout) for more than a month.

--- pkgs/development/libraries/science/math/lcalc/default.nix
+++ pkgs/development/libraries/science/math/lcalc/default.nix
@@ -89,7 +89,6 @@ stdenv.mkDerivation rec {
   ];
 
   meta = with stdenv.lib; {
-    homepage = http://oto.math.uwaterloo.ca/~mrubinst/L_function_public/L.html;
     description = "A program for calculating with L-functions";
     license = with licenses; [ gpl2 ];
     maintainers = with maintainers; [ timokau ];

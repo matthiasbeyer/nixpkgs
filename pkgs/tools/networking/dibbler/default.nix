9cc1b80b6c6e19f78f3270814a3dbcf368b24a99
Matthias Beyer <mail@beyermatthias.de>
2019-07-29 16:20:54 +0200

    Remove link: is dead (connection refused) for more than a month.

--- pkgs/tools/networking/dibbler/default.nix
+++ pkgs/tools/networking/dibbler/default.nix
@@ -17,7 +17,6 @@ stdenv.mkDerivation rec {
 
   meta = with stdenv.lib; {
     description = "Portable DHCPv6 implementation";
-    homepage = http://www.klub.com.pl/dhcpv6/;
     license = licenses.gpl2;
     platforms = platforms.all;
     maintainers = with maintainers; [ fpletz ];

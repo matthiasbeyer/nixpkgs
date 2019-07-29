9373f276143032ce979536a3975542aded419be6
Matthias Beyer <mail@beyermatthias.de>
2019-07-29 16:21:57 +0200

    Remove link: is dead (HTTP error 403) for more than a month.

--- pkgs/development/libraries/idnkit/default.nix
+++ pkgs/development/libraries/idnkit/default.nix
@@ -12,7 +12,6 @@ stdenv.mkDerivation rec {
   buildInputs = [ libiconv ];
 
   meta = with stdenv.lib; {
-    homepage = https://www.nic.ad.jp/ja/idn/idnkit;
     description = "Provides functionalities about i18n domain name processing";
     license = "idnkit-2 license";
     platforms = platforms.linux;

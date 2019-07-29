9e2bebf7c3ae1e2b2d1cf1d8bb58f660ccc6a558
Matthias Beyer <mail@beyermatthias.de>
2019-07-29 16:12:24 +0200

    Remove link: points to Google Code which was discontinued

--- pkgs/development/python-modules/ipaddr/default.nix
+++ pkgs/development/python-modules/ipaddr/default.nix
@@ -16,7 +16,6 @@ buildPythonPackage rec {
 
   meta = with stdenv.lib; {
     description = "Google's IP address manipulation library";
-    homepage = http://code.google.com/p/ipaddr-py/;
     license = licenses.asl20;
   };
 

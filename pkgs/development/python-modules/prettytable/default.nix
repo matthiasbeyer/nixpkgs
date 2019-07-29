0e296549600bc216e91d588ac6d1e9952b7ad51e
Matthias Beyer <mail@beyermatthias.de>
2019-07-29 16:12:27 +0200

    Remove link: points to Google Code which was discontinued

--- pkgs/development/python-modules/prettytable/default.nix
+++ pkgs/development/python-modules/prettytable/default.nix
@@ -21,7 +21,6 @@ buildPythonPackage rec {
 
   meta = with stdenv.lib; {
     description = "Simple Python library for easily displaying tabular data in a visually appealing ASCII table format";
-    homepage = http://code.google.com/p/prettytable/;
     license = licenses.bsd3;
   };
 

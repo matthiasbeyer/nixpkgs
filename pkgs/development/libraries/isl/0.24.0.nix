6c2ffd5d7546c46e19fcdc44ffe2b06983370b17
Matthias Beyer <mail@beyermatthias.de>
2021-11-20 10:25:20 +0100

    fixup! libisl: add 0.24

--- pkgs/development/libraries/isl/0.24.0.nix
+++ pkgs/development/libraries/isl/0.24.0.nix
@@ -3,8 +3,8 @@
 callPackage ./generic.nix rec {
   version = "0.24";
   urls = [
-    "mirror://sourceforge/libisl/libisl-${version}.tar.xz"
-    "https://libisl.sourceforge.io/libisl-${version}.tar.xz"
+    "mirror://sourceforge/libisl/isl-${version}.tar.xz"
+    "https://libisl.sourceforge.io/isl-${version}.tar.xz"
   ];
   sha256 = "1ak1gq0rbqbah5517blg2zlnfvjxfcl9cjrfc75nbcx5p2gnlnd5";
   homepage = "https://sourceforge.net/projects/libisl/";

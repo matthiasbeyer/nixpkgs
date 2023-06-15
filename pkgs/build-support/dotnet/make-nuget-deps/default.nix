983f56094e080e6e5f583c01e2fbf33f298e4b2f
Evgeny Zemtsov <eze@resoptima.com>
2023-06-15 14:43:46 +0200

    make-nuget-deps: support netrc and private repositories
    Using builtins.fetchurl instead of pkgs.fetchurl makes it possible to
    use netrc file from nix.conf to fetch nuget packages from private
    repositories.
    
    This is a very important change to make the dotnet builder useful in
    the situations when nix is used as a build system for enterprises.
    
    (cherry-picked from d54bab5dea579c9d242cc7c3f11c402c89d93433 "make-nuget-deps: support netrc and private repositories")
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- pkgs/build-support/dotnet/make-nuget-deps/default.nix
+++ pkgs/build-support/dotnet/make-nuget-deps/default.nix
@@ -1,14 +1,16 @@
-{ linkFarmFromDrvs, fetchurl }:
+{ linkFarm, fetchurl }:
 { name, nugetDeps ? import sourceFile, sourceFile ? null }:
-linkFarmFromDrvs "${name}-nuget-deps" (nugetDeps {
-  fetchNuGet = { pname, version, sha256 ? "", hash ? ""
-    , url ? "https://www.nuget.org/api/v2/package/${pname}/${version}" }:
-    fetchurl {
+linkFarm "${name}-nuget-deps" (nugetDeps {
+  fetchNuGet =
+    { pname
+    , version
+    , sha256
+    , url ? "https://www.nuget.org/api/v2/package/${pname}/${version}"
+    }:
+    {
       name = "${pname}.${version}.nupkg";
-      # There is no need to verify whether both sha256 and hash are
-      # valid here, because nuget-to-nix does not generate a deps.nix
-      # containing both.
-      inherit url sha256 hash;
+      inherit url sha256;
+      path = builtins.fetchurl { inherit url sha256; };
     };
 }) // {
   inherit sourceFile;

8e1fccab9fcfb97b6c4f65873b5ecc9531b7c72a
Matthias Beyer <mail@beyermatthias.de>
2024-08-29 10:31:14 +0200

    fixup! make-nuget-deps: support netrc and private repositories

--- pkgs/build-support/dotnet/make-nuget-deps/default.nix
+++ pkgs/build-support/dotnet/make-nuget-deps/default.nix
@@ -4,13 +4,17 @@ linkFarm "${name}-nuget-deps" (nugetDeps {
   fetchNuGet =
     { pname
     , version
-    , sha256
+    , hash
     , url ? "https://www.nuget.org/api/v2/package/${pname}/${version}"
     }:
     {
       name = "${pname}.${version}.nupkg";
-      inherit url sha256;
-      path = builtins.fetchurl { inherit url sha256; };
+      inherit url;
+      sha256 = hash;
+      path = builtins.fetchurl {
+        inherit url;
+        sha256 = hash;
+      };
     };
 }) // {
   inherit sourceFile;
